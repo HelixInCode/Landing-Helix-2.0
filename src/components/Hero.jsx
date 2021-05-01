438fa704e800c88df14885160bbdbd210d70d641
OrlandoMendoza17 <ommv.17@gmail.com>
2021-05-01 14:37:49 -0200

    Instalación de typed.js

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
+import Typed from 'typed.js'
+import {options} from '../utils/typed';
 import Button from '../widgets/Button';
 
 const Hero = () => {
+  const titleTyped = useRef(null)
+  
+  useEffect(() => {
+    console.log(titleTyped.current)    
+    var typed = new Typed(titleTyped.current, options);
+  }, [])
+  
   return (
     <section id="inicio" className="Hero">
       <div className="Hero__container">
         
         <h1 className="Hero-title">
-          <span className="Hero-title__main typed">Web Design</span>
+          <span className="Hero-title__main">
+            <span ref={titleTyped}></span> 
+          </span>
           <span className="Hero-title__secondary">Tu futuro ahora</span>
         </h1>
         
         <div className="Hero-buttons">
-          
           <a href="#servicios">
             <Button color="dark">Servicios</Button>
           </a>
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
